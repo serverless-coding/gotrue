15e17d2af994fc2b2d4d98c88c8637b53943580d
yiGmMk <marvelousme@163.com>
2023-06-01 12:06:47 +0000

    script

--- build.sh
+++ build.sh
@@ -6,21 +6,10 @@ mkdir -p "$(pwd)/functions"
 mkdir -p "$(pwd)/functions/register"
 mkdir -p "$(pwd)/functions/settings"
 
-# make build
-# GOBIN=$(pwd)/functions go install ./...
-# chmod +x "$(pwd)"/functions/*
-
-pushd apis/register
-go build -o gotrueRegister -ldflags "-X github.com/netlify/gotrue/cmd.Version=`git rev-parse HEAD`"
-GOBIN=$PATH_PROJECT/functions/register go install ./...
-chmod +x "$PATH_PROJECT"/functions/register/*
-popd
-
-
-pushd apis/settings
-go build -o gotrueSettings -ldflags "-X github.com/netlify/gotrue/cmd.Version=`git rev-parse HEAD`"
-GOBIN=$PATH_PROJECT/functions/settings go install ./...
-chmod +x "$PATH_PROJECT"/functions/settings/*
+pushd functions/setting
+go build -ldflags "-X github.com/netlify/gotrue/cmd.Version=`git rev-parse HEAD`"
+GOBIN=$PATH_PROJECT/functions/setting go install ./...
+chmod +x "$PATH_PROJECT"/functions/setting/*
 popd
 
 ps -aux | grep "go"
